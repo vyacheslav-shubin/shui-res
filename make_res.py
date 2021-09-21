17a7258ee87271e999ed75d66ed66c17810bf41f
Vyacheslav Shubin <shubin-vv@krista.ru>
2021-09-21 20:27:21 +0300

    new color

--- make_res.py
+++ make_res.py
@@ -69,7 +69,7 @@ def color_out(file, json_color):
 def json_build_profile(infn, outfn):
 	print("JSON")
 	import json
-	version=2
+	version=3
 	data=None
 	with open(infn, "r") as json_file:
 		data = json.load(json_file)
@@ -86,6 +86,10 @@ def json_build_profile(infn, outfn):
 		dual_color_out(bin_out, palette["edit_box"])
 		color_out(bin_out, palette["config-line"])
 		color_out(bin_out, palette["dialog-frame"])
+		color_out(bin_out, palette["console-frame"])
+		color_out(bin_out, palette["temperature-point"])
+		color_out(bin_out, palette["temperature-line"])
+
 		while bin_out.tell()<64:
 			bin_out.write(b'\x00')
 		bin_out.close()
